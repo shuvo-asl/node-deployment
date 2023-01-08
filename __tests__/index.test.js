aff82bff68828f26863904eee37f57e6bbc0b854
MD. Mehedi Hasan <shuvo@asl.aero>
2023-01-08 12:46:01 +0600

    Project initialization

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+describe("Sample Test",()=>{
+    it("should test that true === true",()=>{
+        expect(true).toBe(true)
+    })
+})
+
+const request = require('supertest')
+const app = require('../server')
+
+describe('Root Endpoint', () => { 
+    it('shoudl be create a new get request',async () => {
+        const res = await request(app).get('/')
+        expect(res.statusCode).toEqual(200)
+        expect(res.text).toEqual("Hello World!");
+    })
+ })
