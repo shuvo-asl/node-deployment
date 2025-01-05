ccf2d9430bdf014ac0f300f884f90e049b3131eb
MD. Mehedi Hasan <shuvo@asl.aero>
2025-01-05 16:00:07 +0600

    mitigate the framework rist

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app_instance = express();
 
-app_instance.get("/",(req,res)=>{
+// Disable the X-Powered-By header
+app_instance.disable("x-powered-by");
+
+app_instance.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app_instance.get("/about",(req,res)=>{
+app_instance.get("/about", (req, res) => {
   res.send("We are ASL Deployment Team");
 });
 
