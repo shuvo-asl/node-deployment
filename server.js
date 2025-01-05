46bec3de2ebfd61297906e97425659ede2af6966
MD. Mehedi Hasan <shuvo@asl.aero>
2025-01-05 13:20:44 +0600

    update code indentation to raise exception

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const app = require("express");
 
-const server_obj = express();
+const server_obj = app();
 
-server_obj.get("/",(req,res)=>{
-  res.send("Hello World!");
-});
+server_obj.get("/", 
+  (req,res)=>{
+                    res.send("Hello World!");
+}
+);
 
 server_obj.get("/about",(req,res)=>{
   res.send("We are ASL Deployment Team");
